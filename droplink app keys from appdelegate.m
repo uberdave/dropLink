e927d4de38789da870e3d4c72166fc746444f3c2
uberdave <user@fake.domain>
2013-07-03 17:39:41 -0500

    deleted  file

--- droplink app keys from appdelegate.m
+++
@@ -1,8 +0,0 @@
-[Parse setApplicationId:@"Ph2mzUPkUDxP3RJKIr1BdfnJwMOjDpuc0hxvcEGu" clientKey:@"AyenOoH4A5fCj4ll6vO0iQ8aflyauVtLwTNVg9Z6"];
-
-
- DBSession* dbSession =
-    [[DBSession alloc]
-      initWithAppKey:@"90fhsfsbq7xiumy"
-      appSecret:@"k39gsovzgzulhmm"
-      root:kDBRootAppFolder] // either kDBRootAppFolder or kDBRootDropbox
